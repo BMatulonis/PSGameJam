a9ebaedf51f99033c847df6da893bb7d60141f61
Brandon Matulonis <60149447+BMatulonis@users.noreply.github.com>
2024-07-19 00:23:23 -0400

    Improved Movement
    Added double jump and cleaned up movement code and feel

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
-@export var speed : float = 300.0
+@export var speed : float = 150.0
 @export var fall_gravity : float = 1200
-const JUMP_VELOCITY = -400.0
-const JUMP_TIME = 0.15
+const JUMP_VELOCITY = -350.0
+const JUMP_TIME = 0.2
 var timer = 0
+var jumping : bool = false
+var jump_num = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -22,16 +24,27 @@ func _physics_process(delta):
 		velocity.y += get_gravity(velocity) * delta
 	else:
 		timer = 0
+		jump_num = 0
+		jumping = false
 		animation_tree.set("parameters/conditions/idle", 1)
 		animation_tree.set("parameters/conditions/is_jumping", 0)
 		animation_state.travel("idle")
 
+	# handle jumping
 	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y = JUMP_VELOCITY / 4
 
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or timer < JUMP_TIME):
-		timer = JUMP_TIME + 1
+	# double jump
+	if Input.is_action_just_pressed("jump") and !is_on_floor() and jump_num == 1:
+		jump_num += 1
 		velocity.y = JUMP_VELOCITY
+		animation_state.travel("jump")
+
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or timer <= JUMP_TIME) and jump_num < 1:
+		timer = JUMP_TIME + 1
+		jump_num += 1
+		velocity.y = JUMP_VELOCITY
+		jumping = true
 		animation_tree.set("parameters/conditions/idle", 0)
 		animation_tree.set("parameters/conditions/is_jumping", 1)
 		animation_state.travel("jump")
@@ -43,7 +56,14 @@ func _physics_process(delta):
 		$AnimatedSprite2D.flip_h = true
 	if direction:
 		velocity.x = direction * speed
+		animation_tree.set("parameters/conditions/idle", 0)
+		animation_tree.set("parameters/conditions/is_moving", 1)
+		animation_state.travel("move")
 	else:
+		if jumping == false:
+			animation_tree.set("parameters/conditions/idle", 1)
+			animation_tree.set("parameters/conditions/is_moving", 0)
+			animation_state.travel("idle")
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
